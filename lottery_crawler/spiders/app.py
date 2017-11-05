6b90c1734911c3524ea9a7e944c58f0db827a5cc
Jerod <jerodzh@hotmail.com>
2017-11-05 16:16:10 +0800

    itmes

--- lottery_crawler/spiders/app.py
+++ lottery_crawler/spiders/app.py
@@ -7,30 +7,16 @@ from scrapy import Request
 class NgaSpider(scrapy.Spider):
     """ This is the class of the Spider """
     name = "NgaSpider"
-    host = "http://bbs.ngacn.cc/"
+    #host = "http://bbs.ngacn.cc/"
+    allowed_domains = ["www.cnblogs.com"]
 
     start_urls = [
-        "http://bbs.ngacn.cc/thread.php?fid=406"
+        "http://www.cnblogs.com/txw1958/archive/2012/07/16/scrapy-tutorial.html"
     ]
 
     def parse(self, response):
-        for url in self.start_urls:
-            yield Request(url=url, callback=self.parse_page)
-
-    def parse_page(self, response):
-        selector = Selector(response)
-        content_list = selector.xpath("//*[@class='topic']")
-        for content in content_list:
-            topic = content.xpath('string(.)').extract_first()
-            print(topic)
-
-            url = self.host + content.xpath('@href').extract_first()
-            print(url)
-            yield Request(url=url, callback=self.parse_topic)
-
-    def parse_topic(self, response):
-        selector = Selector(response)
-        content_list = selector.xpath("//*[@class='postcontent ubbcode']")
-        for content in content_list:
-            content = content.xpath('string(.)').extract_first()
-            print(content)
+        hxs = Selector(response)
+        title = hxs.xpath('//*[@id="cb_post_title_url"]/text()').extract() 
+        link = hxs.xpath('//*[@id="cb_post_title_url"]/@href').extract()      
+        for t in title:
+            print t.encode('utf-8')
