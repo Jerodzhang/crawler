505cc947344bec086002eeadf8e12559e8312696
Jerod <jerodzh@hotmail.com>
2017-11-07 21:42:37 +0800

    items

--- lottery_crawler/items.py
+++ lottery_crawler/items.py
@@ -4,7 +4,7 @@
 #
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
-
+import scrapy
 from scrapy.item import Item, Field
 
 
--- lottery_crawler/spiders/app.py
+++ lottery_crawler/spiders/app.py
@@ -3,23 +3,35 @@
 import scrapy
 from scrapy import Selector
 from scrapy import Request
+from lottery_crawler.items import LotteryCrawlerItem
 
 class NgaSpider(scrapy.Spider):
     """ This is the class of the Spider """
     name = "NgaSpider"
     #host = "http://bbs.ngacn.cc/"
-    allowed_domains = ["www.cnblogs.com"]
+    allowed_domains = ["dmoztools.net"]
 
     start_urls = [
-        "http://www.cnblogs.com/txw1958/archive/2012/07/16/scrapy-tutorial.html"
+        "http://dmoztools.net/Computers/Programming/Languages/Python/Books/"
     ]
 
     def parse(self, response):
         hxs = Selector(response)
         #title = hxs.xpath('//*[@id="cb_post_title_url"]/text()').extract() 
-        link = hxs.xpath('//*[@id="cb_post_title_url"]/@href').extract()      
+        #link = hxs.xpath('//*[@id="cb_post_title_url"]/@href').extract()      
         ''' for t in title:
             print t.encode('utf-8') '''
-        print "URL: "
+        ''' print "URL: "
         for l in link:
-            print l.encode('utf-8')
+            print l.encode('utf-8') '''
+        sites = hxs.xpath('//*[@class="title-and-desc"]')
+        items = []
+        for site in sites:            
+            item = LotteryCrawlerItem()
+            item['title'] = site.xpath('a/div/text()').extract()
+            item['link'] = site.xpath('a/@href').extract()
+            item['desc'] = site.xpath('div/text()').extract()
+            items.append(item)
+        return items
+            #print "desc: " 
+            #print desc
